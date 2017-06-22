cc82b784b41d04f8179b62b947ef12942b78c84d
Young Park <young_park@brown.edu>
2017-06-22 22:19:30 +0900

    Skeleton layout

--- src/LostCow.java
+++ src/LostCow.java
@@ -1,6 +1,26 @@
 /**
- * Created by young on 6/22/17.
+ * Created by Young Park
+ * For practice
  */
 public class LostCow {
-    
+
+    private int moveNum;
+    private int farmerJohn;
+    private int cowBessie;
+    private boolean trackOperator;
+
+    public LostCow(int farmer, int cow) {
+        this.moveNum = 1; // used to get increment for addition/subtraction
+        this.farmerJohn = farmer; // retrieves John's position
+        this.cowBessie = cow; // retrieves Bessie's position
+        this.trackOperator = true; // initially positive for adding John's position; false = negative.
+    }
+
+    /*
+    Finds the next number to add or subtract to/from John's position.
+     */
+    private void increment() {
+        this.moveNum = this.moveNum * 2;
+    }
+
 }
